eeabb8cba77ec8d7880ff1342dd13ea2b8d2c719
leo_RPi <leonid.shekhovtsev@gmail.com>
2026-09-15 15:55:28 +0300

    шим-диммирование светодиода

---
+++ get-led/pwm-led.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+led=26
+
+GPIO.setup(led, GPIO.OUT)
+
+
+pwm=GPIO.PWM(led, 200)
+duty=0.0
+pwm.start(duty)
+
+while True:
+    pwm.ChangeDutyCycle(duty)
+    time.sleep=1.0
+
+    duty+=0.0001
+    if duty>100.0:
+        duty=0.0
